da1856051d558b90fdc7eff065daa17931a3b0d7
Sawchin998 <sachin9860810@gmail.com>
2023-04-10 17:38:25 +0545

    feat: configure devise for users

---
+++ db/migrate/20230410110433_add_name_and_gender_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameAndGenderToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :gender, :integer, null: false
+  end
+end
