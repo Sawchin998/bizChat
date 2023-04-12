07f8d229dee33889f0c9ceeef311c85c5e6a9c96
Sawchin998 <sachin9860810@gmail.com>
2023-04-12 20:10:18 +0545

    chore : modify validation error msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   enum :gender, { male: 0, female: 1, others: 2 }
   validates :email, format: { with: /\A[\w+\-.]+@bajratechnologies.com\z/i,
-    message: "Only Bajra Technologies email addresses are allowed" }
+    message: "of Bajra Technologies are allowed" }
   validates :name, presence: true, length: { minimum: 2 }
   validates :gender, presence: true
   validates :password, length: { minimum: 8 }
