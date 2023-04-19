2eb99c31364f40da9aeb652946c70ab0aa9f5df7
Sawchin998 <sachin9860810@gmail.com>
2023-04-19 23:46:48 +0545

    refactor:  registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   root "home#index"
   resources :messages
   resources :groups
+  post '/home/search', to: 'home#search', as: :search
 end
