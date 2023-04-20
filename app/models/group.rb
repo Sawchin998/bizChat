d8cb2f362135aa1770663a375bd0ce53103775e1
Sawchin998 <sachin9860810@gmail.com>
2023-04-21 00:32:28 +0545

    feat: add group from homepage

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :messages, as: :receiver
   belongs_to :admin, class_name: "User", optional: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 8 }
 end
