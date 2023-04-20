5d07425d37828959060b868aa896e71865e92249
Sawchin998 <sachin9860810@gmail.com>
2023-04-20 11:14:20 +0545

    feat: add serach bar in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,26 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @conversations = conversation
   end
+
+  def search
+    @search = User.where("name LIKE ?",  "%#{params[:query]}%")
+  end
+
+  private
+
+  def conversation
+    @aaa = []
+    @messages = Message.where("user_id = ? OR receiver_id = ?", current_user.id, current_user.id)
+    # @messages.each do |msg|
+    #   if msg[:receiver_type] == "Group"
+        
+    #   else
+
+    #   end
+    
+    @aaa
+
+  end  
 end
