ba6bdb947eb50e721635cf742b0f47a6c5c54efa
Sawchin998 <sachin9860810@gmail.com>
2023-04-23 22:28:36 +0545

    feat: add chat list section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,13 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @conversations = conversation
+    @msg_users = conversation
   end
 
   def search
     @search = User.where("name LIKE ? OR email LIKE ?",  "%#{params[:query]}%", "%#{params[:query]}%")
+                  .where.not(id: current_user.id)
+                  .order("name ASC")
   end
 
   def show
@@ -16,17 +18,17 @@ class HomeController < ApplicationController
   private
 
   def conversation
-    @grp_msg = []
-    @user_msg = []
-    @messages = Message.where("user_id = ? OR receiver_id = ?", current_user.id, current_user.id)
-    @messages.each do |msg|
-      if msg[:receiver_type] == "Group"
-        @grp_msg << msg
-      else
-        @user_msg << msg
-      end
-    end
+    # Get the messages for the current user
+    @messages = Message.where(user_id: current_user.id).or(Message.where(receiver_id: current_user.id))
 
-    @user_msg
-  end  
+    # Join the users table to get the sender and receiver information
+    @users = User.joins("INNER JOIN messages ON users.id = messages.user_id OR users.id = messages.receiver_id")
+              .where(messages: { id: @messages.select(:id) })
+              .where.not(id: current_user.id)
+              .select("users.*, MAX(messages.created_at) AS latest_message_time, messages.id AS latest_message_id")
+              .group("users.id")
+              .order("latest_message_time DESC")
+              .distinct
+  end
+
 end
