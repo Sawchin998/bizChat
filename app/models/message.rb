2a3c9638068f9837c4a3ab9de5e3a130efe52be8
Sawchin998 <sachin9860810@gmail.com>
2023-04-25 12:20:32 +0545

    feat: auto scroll in messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,16 +1,5 @@
 class Message < ApplicationRecord
-  before_validation :normalize_content
   belongs_to :user
   belongs_to :receiver, polymorphic: true
   has_many_attached :images
-  # validates :content, presence: true, if: :content_required?
-
-  private
-  def normalize_content
-    self.content = content.strip if content.present?
-  end
-
-  # def content_required?
-  #   !self.images.present?
-  # end
 end
