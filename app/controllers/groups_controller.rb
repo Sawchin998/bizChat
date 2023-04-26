e79d2a431d2a21f1670971546aea2ca315d2fb0b
Sawchin998 <sachin9860810@gmail.com>
2023-04-26 15:44:22 +0545

    feat: add edit group frame

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
-
-  def index
-    @groups = Group.all
-  end
+  before_action :group_admin?, only: [:edit, :update]
 
   def new
     @group = Group.new
@@ -19,9 +16,22 @@ class GroupsController < ApplicationController
 
     if @group.save
       @group.users << current_user
-      redirect_to groups_url
+      redirect_to @group, notice: "Group was successfully created."
     else
       render "new", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to @group, notice: "Group was successfully updated."
+    else
+      render :edit
     end
   end
 
@@ -29,5 +39,13 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:name, :description)
+  end
+
+  def group_admin?
+    @group = Group.find(params[:id])
+    unless current_user.id == @group.admin_id
+      flash[:error] = "You do not have permission to access this page."
+      redirect_to @group
+    end
   end
 end
